af5d04999a0873fd051017e15972115acb074b18
Prince Kushwaha <251311931+princekushwaha9142@users.noreply.github.com>
2026-01-27 14:31:21 +0530

    datatypes

---
+++ Python Lesson 2: Variables & Data Types/datatypes.py
@@ -0,0 +1,32 @@
+# Python Data Types 
+# 1. Integer (int)
+
+# Whole numbers
+
+x = 10
+y = -5
+
+# 2. Float (float)
+
+# Decimal numbers
+
+price = 99.99
+pi = 3.14
+
+# 3. String (str)
+
+# Text inside quotes
+
+name = "Prince"
+city = 'Delhi'
+
+# Quotes are mandatory
+
+# 🔹 4. Boolean (bool)
+
+# Only True or False
+
+is_student = True
+is_admin = False
+
+# Capital T and F only
