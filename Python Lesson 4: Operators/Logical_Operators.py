53dee464b3700d3a2146b8485435d2b2ebe9b8ff
Prince Kushwaha <princekushwa9142@gmail.com>
2026-01-29 16:37:52 +0530

    Logical Operators

---
+++ Python Lesson 4: Operators/Logical_Operators.py
@@ -0,0 +1,11 @@
+age = 20
+is_student = True
+
+print(age >= 18 and is_student)
+print(age >= 18 or is_student)
+print(not is_student)
+
+# Meaning:
+# and → both must be True
+# or → at least one True
+# not → reverse
