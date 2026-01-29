b041129b3db9d7f321bd6d51cd97660c6d2f236a
Prince Kushwaha <princekushwa9142@gmail.com>
2026-01-29 16:44:15 +0530

    Assignment Operators

---
+++ Python Lesson 4: Operators/Assignment_Operators.py
@@ -0,0 +1,12 @@
+x = 10        # x starts with value 10
+
+x += 5        # same as x = x + 5 → 10 + 5 = 15
+print(x)     # 15
+
+x -= 3        # same as x = x - 3 → 15 - 3 = 12
+print(x)     # 12
+
+x *= 2        # same as x = x * 2 → 12 * 2 = 24
+print(x)     # 24
+
+
