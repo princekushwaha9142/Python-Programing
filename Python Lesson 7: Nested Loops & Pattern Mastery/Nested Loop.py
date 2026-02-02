a8b77732c972c3705e27244981f373a05b1ba8f9
Prince Kushwaha <princekushwa9142@gmail.com>
2026-02-02 08:36:00 +0530

    Nested Loop

---
+++ Python Lesson 7: Nested Loops & Pattern Mastery/Nested Loop.py
@@ -0,0 +1,17 @@
+# A loop inside another loop.
+
+for i in range(3):
+    for j in range(2):
+        print(i, j)
+
+# Outer loop = rows
+# Inner loop = columns
+# ------------------------
+
+# Pattern Printing Basics
+
+# Rule:
+
+# Outer loop → number of lines
+
+# Inner loop → things printed in each line
