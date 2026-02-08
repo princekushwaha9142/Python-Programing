26b350c8a884bd14cd8af92f7d942b196854cfdc
Prince Kushwaha <princekushwa9142@gmail.com>
2026-02-08 09:19:29 +0530

    String Operations

---
+++ Python Lesson 9: Strings (str)/String Operations.py
@@ -0,0 +1,16 @@
+# Operations
+
+# Concatenation
+
+a = "Hello"
+b = "World"
+print(a + " " + b)
+
+# Repetition
+
+print("Hi " * 3)
+
+# Membership
+
+print("Py" in "Python")     # True
+print("java" in "Python")  # False
