f4cdf90de56e89907fe8e1b7a4957c22218574e1
Prince Kushwaha <princekushwa9142@gmail.com>
2026-02-08 08:54:47 +0530

    Strings

---
+++ Python Lesson 9: Strings (str)/String Indexing.py
@@ -0,0 +1,13 @@
+# Each character has an index.
+
+s = "Python"
+# P	   y   t	 h	  o	  n
+# 0	   1   2	 3	  4	  5
+# -6  -5  -4	-3	 -2	 -1
+
+
+print(s[0])    # P
+print(s[3])    # h
+print(s[-1])   # n
+
+# Strings are 0-indexed
---
+++ Python Lesson 9: Strings (str)/string.py
@@ -0,0 +1,9 @@
+# string is a sequence of characters inside quotes
+
+name = "Prince"
+city = 'Delhi'
+
+# Type check:
+
+print(type(name))   # <class 'str'>
+print(type(city))   # <class 'str'>
