6d8d157817b9cc2d06a7aef10233ed552440b2d5
Prince Kushwaha <princekushwa9142@gmail.com>
2026-02-10 23:13:16 +0530

    Count vowels and consonants

---
+++ Python Lesson 9: Strings (str)/08_practice.py
@@ -0,0 +1,16 @@
+# Count vowels and consonants
+
+s = input("Enter a string: ").lower()
+
+vowels = 0
+consonants = 0
+
+for ch in s:
+    if ch.isalpha():
+        if ch in "aeiou":
+            vowels += 1
+        else:
+            consonants += 1
+
+print("Vowels:", vowels)
+print("Consonants:", consonants)
