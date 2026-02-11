afd61c03673aa4f982aab4a6658c9713ace78300
Prince Kushwaha <princekushwa9142@gmail.com>
2026-02-11 17:29:31 +0530

    list

---
+++ Python Lesson 10: Lists (list)/list.py
@@ -0,0 +1,7 @@
+# list is a collection of multiple values.
+# Lists can store different data types
+
+numbers = [10, 20, 30, 40]
+names = ["Prince", "Alex", "John"]
+mixed = [10, "Hello", True, 3.14]
+
