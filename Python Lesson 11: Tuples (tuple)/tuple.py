8a0ee348a51c291d491333eaee91e1800e296719
Prince Kushwaha <princekushwa9142@gmail.com>
2026-02-13 23:56:01 +0530

    tuple

---
+++ Python Lesson 11: Tuples (tuple)/tuple.py
@@ -0,0 +1,8 @@
+# tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+# tuple is written with round brackets.
+# tuple is like a list…
+
+numbers = (10, 20, 30)
+names = ("Prince", "Alex")
+mixed = (10, "Hello", True)
+
