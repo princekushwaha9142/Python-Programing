074d88aef7c707036c7e3ad9a628cc9e72f125eb
Prince Kushwaha <princekushwa9142@gmail.com>
2026-02-14 22:08:31 +0530

    Tuple Slicing

--- Python Lesson 11: Tuples (tuple)/Tuple Slicing.py
+++ Python Lesson 11: Tuples (tuple)/Tuple Slicing.py
@@ -4,3 +4,16 @@ t[1:4]  # Output: (20, 30, 40)
 t[:3]   # Output: (10, 20, 30)
 t[2:]   # Output: (30, 40, 50)
 t[-3:-1] # Output: (30, 40)
+
+
+
+# Tuples are IMMUTABLE 
+
+# NOT allowed:
+
+a = (10, 20, 30)
+a[0] = 99    # ERROR  
+
+# Python error:
+
+# TypeError: 'tuple' object does not support item assignment
