129dca1bc3ebbcfdc17032b10fa7d8da9efaef4e
Prince Kushwaha <princekushwa9142@gmail.com>
2026-02-14 22:26:27 +0530

    Count occurrences of a number

---
+++ Python Lesson 11: Tuples (tuple)/03_practice.py
@@ -0,0 +1,8 @@
+# Count occurrences of a number
+
+t = (10, 20, 30, 10, 40, 10)
+print(t.count(10))  # Output: 3
+
+# Find index of a number
+t = (10, 20, 30, 10, 40, 10)
+print(t.index(20))  # Output: 1
