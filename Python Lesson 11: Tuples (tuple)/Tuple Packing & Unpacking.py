47ce2b8329c6d805f4b8f2040fc11728f8f8e7a5
Prince Kushwaha <princekushwa9142@gmail.com>
2026-02-14 22:11:05 +0530

    Tuple Packing & Unpacking

---
+++ Python Lesson 11: Tuples (tuple)/Tuple Packing & Unpacking.py
@@ -0,0 +1,17 @@
+# Packing
+
+t = 10, 20, 30
+
+# Python automatically creates tuple 
+
+# Unpacking 
+
+t = (10, 20, 30)
+
+a, b, c = t
+
+print(a)
+print(b)
+print(c)
+
+# Used everywhere in real Python
