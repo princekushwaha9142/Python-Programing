8fc34b073fd06aef320ec80b23ed1294413dfc60
Prince Kushwaha <251311931+princekushwaha9142@users.noreply.github.com>
2026-06-12 18:37:32 +0530

    Fix assignment operator example in Python lesson

--- Python Lesson 4: Operators/Assignment_Operators.py
+++ Python Lesson 4: Operators/Assignment_Operators.py
@@ -6,7 +6,7 @@ print(x)     # 15
 x -= 3        # same as x = x - 3 → 15 - 3 = 12
 print(x)     # 12
 
-x *= 2        # same as x = x * 2 → 12 * 2 = 24
-print(x)     # 24
+x *= 2        # same as x = x * 2 → 10 * 2 = 20
+print(x)     #  20
 
 
