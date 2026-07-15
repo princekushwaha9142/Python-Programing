517dfb412e32ec1a6e105877388e62417e89f81d
Prince Kushwaha <251311931+princekushwaha9142@users.noreply.github.com>
2026-07-16 00:57:57 +0530

    Fix assignment operator examples in Python script

--- Python Lesson 4: Operators/Assignment_Operators.py
+++ Python Lesson 4: Operators/Assignment_Operators.py
@@ -3,8 +3,8 @@ x = 10        # x starts with value 10
 x += 5        # same as x = x + 5 → 10 + 5 = 15
 print(x)     # 15
 
-x -= 3        # same as x = x - 3 → 15 - 3 = 12
-print(x)     # 12
+x -= 3        # same as x = x - 3 → 10 - 3 = 7
+print(x)     # 7
 
 x *= 2        # same as x = x * 2 → 10 * 2 = 20
 print(x)     #  20
